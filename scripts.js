78647c0cbe5ba00d32993416507d4a5e3ee8ba71
starrymeadows <aliharris.star@gmail.com>
2022-10-25 21:40:47 -0400

    Add logic for marker placement

--- scripts.js
+++ scripts.js
@@ -1,51 +1,79 @@
 const gameboard = (() => {
     // gameboard array
-    const board = ['x', 'o', 'x', 'o', 'x', 'x', 'x', 'o', 'o'];
+    const board = ['', '', '', '', '', '', '', '', ''];
 
     // update array
+    const updateBoard = (index, marker) => {
+        board.splice(index, 1, marker);
+    }
 
     return {
         board,
+        updateBoard,
     }
 })();
 
 // needs access to: board, players, winstate
 const displayController = ((currentBoard) => {
+    const cells = document.querySelectorAll('.cell');
     // draw the gameboard
-    const initializeBoard = (currentBoard) => {
-        const cells = document.querySelectorAll('.cell');
+    const initializeBoard = () => {
+        for (i = 0; i < cells.length; i++) {
+            // get the index of the cell
+            let cell = cells.item(i);
+            let index = i;
+            // append content to the cell according to corresponding index of board
+            cell.addEventListener('click', () => gameLogic.placeMarker(index));
+        };
+    };
+
+    // draw players
+
+    // update gameboard
+    const updateBoard = () => {
         for (i = 0; i < cells.length; i++) {
             // get the index of the cell
             let cell = cells.item(i);
             // append content to the cell according to corresponding index of board
-            cell.textContent = currentBoard[i];
-        };
-    };
+            cell.textContent = gameboard.board[i];
+        }
+    }
+
+    // display win message
+
+    // add replay button
+
+    // reset gameboard
 
     return {
         initializeBoard,
+        updateBoard,
     }
-    // draw players
-
-    // update gameboard
-
-    // display win message
 
 })(board);
 
 
 // needs access to: board, players
-// const gameLogic = (() => {
-//     // determine current player
+const gameLogic = (() => {
+    const marker = 'z';
+    // determine current player
 
-//     // place marker (call to update array, update display)
+    // place marker (call to update array, update display)
+    const placeMarker = (index) => {
+        if (gameboard.board[index] === '') {
+            gameboard.updateBoard(index, marker);
+            displayController.updateBoard();
+        }
+    }
 
-//     // determine win / tie
+    // determine win / tie
 
-//     // swap current player
+    // swap current player
 
-//     // add replay button
-// })();
+    return {
+        placeMarker,
+    }
+})();
 
 // const Players = (playerName, marker, id) = {
 //     // return new player
